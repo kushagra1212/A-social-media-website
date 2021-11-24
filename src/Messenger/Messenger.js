d12a4225bcc8e763a3021e3f08618d14c1fefa44
kushagra1212 <kushagrarathore002@gmail.com>
2021-11-24 21:13:18 +0530

    ss

--- src/Messenger/Messenger.js
+++ src/Messenger/Messenger.js
@@ -34,7 +34,7 @@ const Messenger = () => {
         const conver = await getconversations(username);
      
         let socket = io(ENDPOINT, {
-          transports: ["polling"],
+          transports: ["websocket", "polling", "flashsocket"],
         });
 
         dispatch(setsocket(socket));
