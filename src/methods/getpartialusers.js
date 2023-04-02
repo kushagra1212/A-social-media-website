e59e594d4cdb38901492528a357f511e8c1890aa
unknown <kushagrarathore002@gmail.com>
2023-04-02 17:58:14 +0530

    fix(bug): suggest user bug

--- src/methods/getpartialusers.js
+++ src/methods/getpartialusers.js
@@ -1,15 +1,14 @@
-import axios from "axios";
+import axios from 'axios';
 const URL = process.env.REACT_APP_URL;
-const getpartialusers=(username)=>{
-        
-        return  new Promise(async(resolve,reject)=>{
-                try{    
-                        const res=await axios.get(`${URL}/users/searchuser/${username}`);
-                        resolve(res.data);
-                }catch(err){
-                        reject(err)
-                }
-        })
-}
+const getpartialusers = (username) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const res = await axios.get(`${URL}/users/searchuser/${username}`);
+      resolve(res.data);
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
 
 export default getpartialusers;
